3b71e66538090af0182120b43dca1e8ad850a7f2
Firu1996 <chayakorn.f.ch@gmail.com>
2022-07-17 15:16:33 +0700

    handleAddToCart for non login

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { addToCart } from '../store/slices/cartSlice'
-import { useDispatch } from 'react-redux'
-
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 export default function Item({ product }) {
+  const { user } = useSelector(state => state.auth)
   const dispatch = useDispatch()
-
+  const history = useHistory()
+  const handleAddToCart = () => {
+    if (user) {
+      dispatch(addToCart({ ...product, qty: 1 }))
+    } else {
+      history.push('/signin')
+    }
+  }
 
   return (
     <div className='product' >
       <h4>{product.title}</h4>
       <p>{product.price}</p>
-      <button onClick={() => dispatch(addToCart({ ...product, qty: 1 }))}> Add To Cart</button>
+      <button onClick={handleAddToCart}> Add To Cart</button>
     </div>
   )
 }
