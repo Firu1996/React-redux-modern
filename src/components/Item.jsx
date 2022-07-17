c9cb7478e5f4334141b7122f7a12e2ff5b29cf85
Firu1996 <chayakorn.f.ch@gmail.com>
2022-07-17 14:39:06 +0700

    add dispatch addToCart

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { addToCart } from '../store/slices/cartSlice'
+import { useDispatch } from 'react-redux'
 
-export default function Item({product}) {
+export default function Item({ product }) {
+  const dispatch = useDispatch()
+
+
   return (
     <div className='product' >
-        <h4>{product.title}</h4>
-        <p>{product.price}</p>
-        <button> Add To Cart</button>
-      </div>
+      <h4>{product.title}</h4>
+      <p>{product.price}</p>
+      <button onClick={() => dispatch(addToCart({ ...product, qty: 1 }))}> Add To Cart</button>
+    </div>
   )
 }
