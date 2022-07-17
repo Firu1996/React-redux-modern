fcfd4410b62d3734a5cf1ce33c7a9add1b1a1940
Firu1996 <chayakorn.f.ch@gmail.com>
2022-07-17 14:55:10 +0700

    Update authSlice.js

--- src/store/slices/authSlice.js
+++ src/store/slices/authSlice.js
@@ -1,13 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { signin } from '../../data/users'
 
 const initialState = {
-    user: null
+    user: null,
+    loading: false,
+    error: ''
 }
 
-const userSlice = createSlice({
+const signInAsync = createAsyncThunk('signin', async ({ email, password, store }) => {
+
+    try {
+        const user = await signin(email, password)
+        return user // using this value by calling action.payload 
+    } catch (error) {
+        throw error // using this value by calling action.payload 
+    }
+})
+
+// signInAsync.pending , signInAsync.fulfilled, signInAsync.rejected  automation create status by createAsyncThunk
+
+const authSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-
-    }
+        signout: (state, action) => {
+            state.user = null
+            state.loading = false
+            state.error = ''
+        }
+    },
+    extraReducers: {
+        [signInAsync.pending]: (state, action) => {
+            state.loading = true
+            state.error = ''
+        },
+        [signInAsync.fulfilled]: (state, action) => {
+            state.user = action.payload
+            state.loading = false
+            state.error = ''
+        },
+        [signInAsync.rejected]: (state, action) => {
+            state.loading = false
+            state.error = action.error.message
+        }
+    },
 })
+
+
+export const { signout } = authSlice.actions
+
+export default authSlice.reducer
