ad4316616cbfd5031e7bc1eb72a787e29fc07dc6
Firu1996 <chayakorn.f.ch@gmail.com>
2022-07-17 15:16:51 +0700

    add signin

--- src/pages/SIgnin.jsx
+++ src/pages/SIgnin.jsx
@@ -1,14 +1,21 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { signInAsync } from '../store/slices/authSlice'
 
 export default function Signin() {
-  const [username, setUsername] = useState('')
+  const { loading, error } = useSelector(state => state.auth)
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-
+  const dispatch = useDispatch()
   return (
     <div className='form'>
-      <input type="text" name='username' value={username} onChange={e => setUsername(e.target.value)} />
+      <input type="text" name='email' value={email} onChange={e => setEmail(e.target.value)} />
       <input type="password" name='password' value={password} onChange={e => setPassword(e.target.value)} />
-      <button>Submit</button>
+      {!loading ?
+        <button onClick={() => dispatch(signInAsync({ email, password }))}>Submit</button>
+        : <button disabled>Loading</button>
+      }
+      {error.length > 0 && <p>{error}</p>}
     </div>
   )
 }
