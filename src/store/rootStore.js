a347d82aa05d019d50a993b266ef2f858e0e5cca
Firu1996 <chayakorn.f.ch@gmail.com>
2022-07-17 14:55:14 +0700

    Update rootStore.js

--- src/store/rootStore.js
+++ src/store/rootStore.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import cartReducer from './slices/cartSlice'
+import authReducer from './slices/authSlice'
 
 export const rootStore = configureStore({
     reducer: {
         cart: cartReducer,
-
+        auth: authReducer
     }
 })
 // { cart: [] }
